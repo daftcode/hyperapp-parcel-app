cb90476fcb18b574a29ef83c0189b16133a640dc
Adam Borowski <adam@adamboro.com>
2018-01-14 19:31:45 +0000

    annotate code

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const getUserDataFn = username => {
 
 const getUserData = debounce(getUserDataFn, 700)
 
+// single global state - one per app
 const state = {
   username: '',
   userData: null,
@@ -17,12 +18,21 @@ const state = {
 
 const actions = {
   updateUsername: (username) => (state, actions) => {
-    getUserData(username).then(actions.setUserData)
+    // perform side effect - fetching the user data from github API
+    getUserData(username)
+      .then(actions.setUserData)
+    //  what the action actually changes in state is just username
     return { username }
   },
+  // a simplest action, which just updates some part of state
   setUserData: userData => state => ({ userData })
 }
 
+// here comes the JSX, but remember that it's just syntactic sugar:
+// <div className='classy'>hello</div>
+// becomes
+// h('div', {className: 'classy'}, 'hello')
+// the 'h' corresponds to 'React.createElement' in React
 const view = (state, actions) =>
   <main>
     <div>Search github users:</div>
@@ -46,4 +56,9 @@ const view = (state, actions) =>
     </div>
   </main>
 
+// This runs the app. The return object (here not used) makes it possible
+// to interact with the app from outside of the view
+// In a real app, you should not attach the view directly to the body, but
+// create some element in the static HTML to attach to.
+// more on that: https://medium.com/@dan_abramov/two-weird-tricks-that-fix-react-7cf9bbdef375#486f
 app(state, actions, view, document.body)
